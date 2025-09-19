ffbd2844ee4cb3e29cdcf1e1b0e88d3e0606134b
MariaLuizazz <souzaluizamaria57@gmail.com>
2025-09-19 08:29:13 -0300

    main

---
+++ docs/projeto1/Exploracaodedados.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from io import StringIO
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from tabulate import tabulate  
+
+
+#carregamento da base
+df = pd.read_csv('https://raw.githubusercontent.com/bligui/Machine-Learning-Projetos/refs/heads/main/base/Obesity%20Classification.csv')
+print(df.sample(n=10, random_state=42).to_markdown(index=False))
